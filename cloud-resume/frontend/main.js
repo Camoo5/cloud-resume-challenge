13e973f24fc99928ed5e913e121902242ad07ac8
Camoo5 <oluwoleokanlawon91@gmail.com>
2024-09-10 20:53:51 +0100

    ProductionApi

--- cloud-resume/frontend/main.js
+++ cloud-resume/frontend/main.js
@@ -2,18 +2,19 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApiURL = 'https://getvisitorcounter.azurewebsites.net/api/GetVisitorCounter'?
-const localFunctionApi ='http://localhost:7071/api/GetVisitorCounter';
 
-function getVisitCount() {
+const productionApiUrl = 'https://getvisitorcounter.azurewebsites.net/api/GetVisitorCounter?';
+const localApiUrl = 'http://localhost:7071/api/GetVisitorCounter';
+
+const getVisitCount= () => {
     let count = 30;
-    fetch('functionApiUrl').then(response => {
-        return response.json();
-    }).then(response => {
+    fetch('https://getvisitorcounter.azurewebsites.net/api/GetVisitorCounter?').then(response => {
+        return response.json()
+    }).then(response =>{
         console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
-    }).catch(function (error) {
+    }).catch(function (error){
         console.log(error);
     });
     return count;
