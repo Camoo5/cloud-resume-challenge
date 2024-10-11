35c8fde0e80e147a4f93417fc9b88f9a7b657063
Camoo5 <oluwoleokanlawon91@gmail.com>
2024-10-11 13:08:51 +0100

    Delete deployment

--- .github/workflows/delete_a_deployment.sh
+++ .github/workflows/delete_a_deployment.sh
@@ -1,8 +1,13 @@
-# GitHub CLI api
-# https://cli.github.com/manual/gh_api
+#!/bin/bash
 
-gh api \
-  --method DELETE \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/Camoo5/cloud-resume-challenge/deployments/DEPLOYMENT_ID
+REPO="Camoo5/cloud-resume-challenge"
+STATUS="inactive" # or any other status you want to filter by
+
+# Get deployments with the specified status
+DEPLOYMENTS=$(gh api -H "Accept: application/vnd.github+json" -X GET "/repos/$REPO/deployments" | jq -r --arg status "$STATUS" '.[] | select(.status == $status) | .id')
+
+# Loop through each deployment ID and delete it
+for DEPLOYMENT_ID in $DEPLOYMENTS; do
+  echo "Deleting deployment $DEPLOYMENT_ID"
+  gh api --method DELETE -H "Accept: application/vnd.github+json" "/repos/$REPO/deployments/$DEPLOYMENT_ID"
+done
