d2e332ad11c11708b8d61fd626b5e99b90b345c6
Camoo5 <oluwoleokanlawon91@gmail.com>
2024-10-11 17:58:50 +0100

    Delete Delopyments

--- .github/workflows/delete_inactive_deployments.sh
+++ .github/workflows/delete_inactive_deployments.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 REPO="Camoo5/cloud-resume-challenge"
-STATUS="main" # or any other status you want to filter by
+BRANCH="main"
 
-# Get deployments with the specified status
-DEPLOYMENTS=$(gh api -H "Accept: application/vnd.github+json" -X GET "/repos/$REPO/deployments" | jq -r --arg status "$STATUS" '.[] | select(.environment == $status) | .id')
+# Get all deployments associated with the main branch
+DEPLOYMENTS=$(gh api -H "Accept: application/vnd.github+json" -X GET "/repos/$REPO/deployments" | jq -r --arg branch "$BRANCH" '.[] | select(.ref == $branch) | .id')
 
 # Loop through each deployment ID and delete it
 for DEPLOYMENT_ID in $DEPLOYMENTS; do
   echo "Deleting deployment $DEPLOYMENT_ID"
   gh api --method DELETE -H "Accept: application/vnd.github+json" "/repos/$REPO/deployments/$DEPLOYMENT_ID"
 done
-
