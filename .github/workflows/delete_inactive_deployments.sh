845e1cc5e20179702055fc3fdfeb98ec69c5ab3d
Camoo5 <oluwoleokanlawon91@gmail.com>
2024-10-11 18:23:37 +0100

    Delete

--- .github/workflows/delete_inactive_deployments.sh
+++ .github/workflows/delete_inactive_deployments.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 REPO="Camoo5/cloud-resume-challenge"
-BRANCH="main"
+STATUS="main" # or any other status you want to filter by
 
-# Get all deployments associated with the main branch
-DEPLOYMENTS=$(gh api -H "Accept: application/vnd.github+json" -X GET "/repos/$REPO/deployments" | jq -r --arg branch "$BRANCH" '.[] | select(.ref == $branch) | .id')
+echo "Fetching deployments from repo: $REPO with status: $STATUS"
+
+# Get deployments with the specified status
+DEPLOYMENTS=$(gh api -H "Accept: application/vnd.github+json" -X GET "/repos/$REPO/deployments" | jq -r --arg status "$STATUS" '.[] | select(.environment == $status) | .id')
+
+echo "Deployments to delete: $DEPLOYMENTS"
 
 # Loop through each deployment ID and delete it
 for DEPLOYMENT_ID in $DEPLOYMENTS; do
   echo "Deleting deployment $DEPLOYMENT_ID"
-  gh api --method DELETE -H "Accept: application/vnd.github+json" "repos/$REPO/deployments/$DEPLOYMENT_ID"
+  gh api --method DELETE -H "Accept: application/vnd.github+json" "/repos/$REPO/deployments/$DEPLOYMENT_ID"
 done
