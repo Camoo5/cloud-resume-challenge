3d4b6e8949d833d8128a31881bfa95475d727d94
Camoo5 <oluwoleokanlawon91@gmail.com>
2024-12-07 14:44:16 +0000

    Capstone project

---
+++ backend/api/Counter.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text.Json.Serialization;
+namespace My.Namespace
+{
+    public class Counter(string id, int count)
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = id ?? throw new ArgumentNullException(nameof(id));
+
+        [JsonPropertyName("count")]
+        public int Count { get; set; } = count;
+    }
+}
